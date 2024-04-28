b7fca381b1ce6c21393695a8f638e5939ef2fc74
masonstudent <prongerm@myumanitoba.ca>
2024-04-28 16:31:33 -0500

    Added implementations to most process functions

--- Inc/motor_controller_can_utils.h
+++ Inc/motor_controller_can_utils.h
@@ -7,6 +7,8 @@
 
 //Defines
 
+#define MC_GEN5             0
+
 #define TORQUE_MODE			0
 #define SPEED_MODE			1
 
@@ -42,20 +44,88 @@
 
 #define PEAK_TORQUE			230	//Peak torque for EMRAX 228 motor
 
-//Function headers
+//Static Variables
 static int16_t bus_voltage;
 static int16_t bus_current;
 static int16_t mc_currentA;
 static int16_t mc_currentB;
 static int16_t mc_currentC;
+
+//Temp 1
 static int16_t mc_igbtA_temp;
 static int16_t mc_igbtB_temp;
 static int16_t mc_igbtC_temp;
+
+//Temp 2
+static int16_t mc_controlboard_temp;
+static int16_t mc_RTD_temp_1;
+static int16_t mc_RTD_temp_2;
+static int16_t mc_RTD_temp_3;
+
+//Temp 3
+static int16_t mc_coolant_temp;
+static int16_t mc_hot_spot_temp;
 static int16_t mc_motor_temp;
-static int16_t mc_rpm;
-static int16_t mc_output_voltage;
+static int16_t mc_torque_shudder;
+
+//Digital input status
+static int8_t mc_forward_switch;
+static int8_t mc_reverse_switch;
+static int8_t mc_brake_switch;
+static int8_t mc_REGEN_disable;
+static int8_t mc_ignition_switch;
+static int8_t mc_Start_switch;
+static int8_t mc_valet_mode;
+static int8_t mc_digital_input8;
+
+//internal voltages
+static int16_t mc_onehalf_volt_ref;
+static int16_t mc_twohalf_volt_ref;
+static int16_t mc_five_volt_ref;
+static int16_t mc_twelve_volt_ref;
+
+//internal states
+static int8_t mc_VSM_state;
+static int8_t mc_PWM_freq;
+static int8_t mc_inverter_state;
+static int8_t mc_relay_state;
+static int8_t mc_inverter_run_mode;
+static int8_t mc_self_sensing_assist_enable;
+static int8_t mc_inverter_active_discharge_state;
+static int8_t mc_inverter_command_mode;
+static int8_t mc_rolling_counter_value;
+static int8_t mc_inverter_enable_state;
+static int8_t mc_burst_model_mode;
+static int8_t mc_start_mode_active;
+static int8_t mc_inverter_enable_lockout;
+static int8_t mc_direction_command;
+static int8_t mc_BMS_active;
+static int8_t mc_BMS_limiting_torque;
+static int8_t mc_limit_max_speed;
+static int8_t mc_limit_hot_spot;
+static int8_t mc_low_speed_limiting;
+static int8_t mc_coolant_temperature_limiting;
+static int8_t mc_limit_stall_burst_model;
+
+//torque timer info
 static int16_t mc_torque_command;
 static int16_t mc_torque_feedback;
+static int32_t mc_power_on_timer;
+
+//modulation index & flux
+static int16_t mc_modulation_index;
+static int16_t mc_flux_weakening_output;
+static int16_t mc_id_command;
+static int16_t mc_iq_command;
+
+//Motor position info
+static int16_t mc_angle;
+static int16_t mc_rpm;
+static int16_t mc_Electrical_output_freq;
+static int16_t mc_delta_resolver_filtered;
+
+static int16_t mc_output_voltage;
+
 static int16_t mc_vd;
 static int16_t mc_vq;
 
@@ -65,6 +135,16 @@ static int8_t  mc_enable_discharge;
 static int16_t mc_torque_limit;
 static int16_t mc_torque;
 static int16_t mc_speed;
+
+//Firmware information
+static int16_t mc_EEPROM_ver;
+static int16_t mc_software_ver;
+static int16_t mc_datecode_mmdd;
+static int16_t mc_datecode_yyyy;
+
+//torque capability
+static int16_t mc_torque_capability;
+
 
 //Getters
 float mc_getBusVoltage();
@@ -95,7 +175,7 @@ uint8_t isMCBusCharged();
 
 //Can messaging processing
 void mc_process_temp1_can(uint8_t * data);
-void mc_process_temp2_can(uint8_t * data); // This currently does nothing and is only made for the fact that the CAN pdf states that this should exist
+void mc_process_temp2_can(uint8_t * data);
 void mc_process_temp3_can(uint8_t * data);
 void mc_process_analog_inputs_voltage_can(uint8_t * data);
 void mc_process_digital_input_status_can(uint8_t * data);
